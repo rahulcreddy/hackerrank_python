522a4887e684abb7588d5094c39db1e7b1e07322
Rahul Reddy <33981256+rahulcreddy@users.noreply.github.com>
2021-05-03 00:28:57 +0530

    Create 24. Set .intersection() Operation.py

---
+++ 24. Set .intersection() Operation.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __name__ == '__main__':
+    n = int(input())
+    e = set(map(int,input().split()))
+    b = int(input())
+    f = set(map(int,input().split()))
+    
+    print(len(e.intersection(f)))

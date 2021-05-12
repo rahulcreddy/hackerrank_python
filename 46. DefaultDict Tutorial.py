cf5d3c64b098c9ea4255a73977d9575cf393e59b
Rahul Reddy <33981256+rahulcreddy@users.noreply.github.com>
2021-05-12 23:43:27 +0530

    Update 46. DefaultDict Tutorial.py

--- 46. DefaultDict Tutorial.py
+++ 46. DefaultDict Tutorial.py
@@ -3,17 +3,13 @@ from collections import defaultdict
 if __name__ == "__main__":
     n,m = map(int,input().split())
     a = defaultdict(list)
-    b = []
     
     for i in range(1,n+1):
-        temp = input()
-        a[temp].append(i)
-        
-    for i in range(m):
-        b += input()
+        a[input()].append(str(i))
             
     for i in range(m):
-        if not a[b[i]]:
-            print('-1')
-            continue
-        print(" ".join(map(str,a[b[i]])))
+        b = input()
+        if b in a:
+            print(" ".join(a[b]))
+        else:
+            print("-1")

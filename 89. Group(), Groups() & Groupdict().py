92bb90b2ce734e4e065730ab6400623d217d633b
Rahul Reddy <33981256+rahulcreddy@users.noreply.github.com>
2021-06-06 00:13:52 +0530

    Create 89. Group(), Groups() & Groupdict().py

---
+++ 89. Group(), Groups() & Groupdict().py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+
+if __name__ == "__main__":
+    s = input()
+    
+    m = re.search(r'([a-zA-Z0-9])\1+',s)
+    if m:
+        print(m.group(1))
+    else:
+        print("-1")
